88a09b7a985d66101bf4d80cfaa7ba5ab6fff0bb
Frank Lichtenheld <frank@lichtenheld.com>
2025-05-12 16:39:45 +0200

    buildmaster: Delete unused windows build scripts
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>

--- buildbot-host/buildmaster/msbuild-x64.bat
+++
@@ -1,3 +0,0 @@
-rem Building x64 binaries
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
-msbuild /m /p:Configuration=Release /p:Platform="x64" .
