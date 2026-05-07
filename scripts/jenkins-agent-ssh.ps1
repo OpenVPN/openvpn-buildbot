334f1e74553b52a12508032af3d9302e043453bc
Frank Lichtenheld <frank@lichtenheld.com>
2026-05-07 12:39:15 +0200

    scripts: Use Java 21 for Jenkins agents
    Java 17 is not supported anymore in latest Jenkins LTS
    release.
    
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>

--- scripts/jenkins-agent-ssh.ps1
+++ scripts/jenkins-agent-ssh.ps1
@@ -4,7 +4,7 @@ param ([string] $workdir)
 
 Write-Host "Setting up Jenkins agent (SSH)"
 
-& choco.exe install -y openjdk17
+& choco.exe install -y microsoft-openjdk-21
 CheckLastExitCode
 
 if (-Not (Test-Path $workdir)) {
